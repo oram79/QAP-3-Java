21d7529129afc7485c5c37083253aef966697416
Logan Oram <logan.oram@keyin.com>
2024-11-07 18:35:21 -0330

    person object added

---
+++ Question1/Person.java
@@ -0,0 +1,46 @@
+package Question1;
+
+public class Person {
+    protected String myName;
+    protected int myAge;
+    protected String myGender;
+
+    public Person(String name, int age, String gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    // Getter Methods //
+
+    public String getName() {
+        return myName;
+    }
+
+    public int getAge() {
+        return myAge;
+    }
+
+    public String getGender() {
+        return myGender;
+    }
+
+    // Setter Methods //
+
+    public void setName(String name) {
+        myName = name;
+    }
+
+    public void setAge(int age) {
+        myAge = age;
+    }
+
+    public void setGender(String gender) {
+        myGender = gender;
+    }
+
+    public String toString() {
+        return myName + ", age: " + myAge + ", Gender: " + myGender;
+    }
+
+}
