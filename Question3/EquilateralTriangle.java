82ed2c1b0cb1d9b609442be3fd2a08be460e86d6
Logan Oram <logan.oram@keyin.com>
2024-11-08 14:53:51 -0330

    2nd triangle class complete

---
+++ Question3/EquilateralTriangle.java
@@ -0,0 +1,8 @@
+package Question3;
+
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(double side) {
+        super(side, side, side); // All Sides Are Set To Being Equal //
+        super.name = "Equilateral Triangle: ";
+    }
+}
