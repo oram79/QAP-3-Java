f9d23d7b6de56ac2d7fde491b94ca7f9e82101f6
Logan Oram <logan.oram@keyin.com>
2024-11-14 11:42:32 -0330

    Demo done (all shape display in output)

--- Question4/Demo.java
+++ Question4/Demo.java
@@ -4,17 +4,28 @@ public class Demo {
     public static void main(String[] args) {
         Circle circle = new Circle("Circle1", 5);
         Ellipse ellipse = new Ellipse("Ellipse1", 7, 3);
+        Triangle triangle = new Triangle("Triangle1", 3, 4, 5);
+        EquilateralTriangle equilateralTriangle = new EquilateralTriangle("Equilateral1", 5);
+        
+        // listing all shape before scaling //
 
         System.out.println("Before scaling:");
         System.out.println(circle);
         System.out.println(ellipse);
+        System.out.println(triangle);
+        System.out.println(equilateralTriangle);
 
-        // Scale shapes
+        // Scale shapes //
+
         circle.scale(2);
         ellipse.scale(1.5);
+        triangle.scale(2);
+        equilateralTriangle.scale(1.5);
 
         System.out.println("\nAfter scaling:");
         System.out.println(circle);
         System.out.println(ellipse);
+        System.out.println(triangle);
+        System.out.println(equilateralTriangle);
     }
 }
