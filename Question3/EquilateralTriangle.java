2dc337a457ca4c6684d4b559a4beb511015ff578
Logan Oram <logan.oram@keyin.com>
2024-11-20 10:51:52 -0330

    mistake

--- Question3/EquilateralTriangle.java
+++
@@ -1,8 +0,0 @@
-package Question3;
-
-public class EquilateralTriangle extends Triangle {
-    public EquilateralTriangle(double side) {
-        super(side, side, side); // All Sides Are Set To Being Equal //
-        super.name = "Equilateral Triangle: ";
-    }
-}
